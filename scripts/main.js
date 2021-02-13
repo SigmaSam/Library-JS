7d8c0b4129b38ceae3891df1d066af9dd3d151ea
samuel <samuelalmaoherrera@gmail.com>
2021-02-13 20:06:50 -0300

    set up workflow and linters.

--- scripts/main.js
+++ scripts/main.js
@@ -1,38 +1,38 @@
-let myLibrary = [];
+const myLibrary = [];
 
 function Book(title, author, pages, read) {
-    this.title = title
-    this.author = author
-    this.pages = pages
-    this.read = read
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
 }
 
 function addBookToLibrary(book) {
-    myLibrary.push(book)
+  myLibrary.push(book);
 }
 
 function allBooks(arg) {
-    arg.forEach(element => {
-        if(read) {
-            read = 'readed'
-        } else {
-            read = 'no read yet'
-        }
-        return title + ', ' + author+ ', ' + pages + ' pages, ' + read         
-    });
+  arg.forEach(element => {
+    if (element.read) {
+      element.read = 'readed';
+    } else {
+      element.read = 'no read yet';
+    }
+    return `${element.title}, ${element.author}, ${element.pages}pages, ${element.read}`;
+  });
 }
 
-let csDestyled = new Book('Computer Science Destiled', 'Wladston Ferreira Filho', 165, true);
-let hpOne = new Book('Harry Potter and The Philosopher Stone', 'JK Rowlling', 150, true);
-let hpTwo = new Book('Harry Potter and The Chamber of Secrets', 'JK Rowlling', 250, true);
-let hpThree = new Book('Harry Potter and The Azkaban Prisioner', 'JK Rowlling', 350, true);
+const csDestyled = new Book('Computer Science Destiled', 'Wladston Ferreira Filho', 165, true);
+const hpOne = new Book('Harry Potter and The Philosopher Stone', 'JK Rowlling', 150, true);
+const hpTwo = new Book('Harry Potter and The Chamber of Secrets', 'JK Rowlling', 250, true);
+const hpThree = new Book('Harry Potter and The Azkaban Prisioner', 'JK Rowlling', 350, true);
 
 addBookToLibrary(csDestyled);
 addBookToLibrary(hpOne);
 addBookToLibrary(hpTwo);
 addBookToLibrary(hpThree);
 
-console.log(allBooks(myLibrary))
+allBooks(myLibrary);
 
 const library = document.createElement('div');
 document.getElementsByTagName('body')[0].appendChild(library);
