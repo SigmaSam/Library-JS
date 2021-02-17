7f86ddef1ab30d1b8bc779240ecb60cec44679b5
julian3493 <julianramosarevalo@gmail.com>
2021-02-17 16:18:45 -0500

    fix add book issues, add delete button

--- scripts/main.js
+++ scripts/main.js
@@ -10,17 +10,22 @@ function Book(title, author, pages, read) {
 
 // Book Array
 
-const myLibrary = [];
+let myLibrary = []
 
-function addBookToLibrary(book) {
-  if (myLibrary.some((book) => book.title === newBook.title)) return false;
-  myLibrary.push(book);
+if (localStorage.getItem('myLibrary')) {
+  myLibrary = JSON.parse(localStorage.getItem('myLibrary'));
+}
+
+function addBookToLibrary(newBook) {
+  // if (myLibrary.some((book) => book.title === newBook.title)) return false;
+  myLibrary.push(newBook);
   saveLocal();
 }
 
 function removeFromLibrary(bookTitle) {
   myLibrary = myLibrary.filter((book) => book.title !== bookTitle);
   saveLocal();
+  location.reload();
 }
 
 // Books grid.
@@ -46,6 +51,9 @@ function display(library) {
     const markAs = document.createElement('button');
     markAs.textContent = 'Did you read it?';
     markAs.className = 'px-2 py-1 border border-black rounded bg-green-500'
+    const del = document.createElement('button');
+    del.textContent = 'Delete';
+    del.className = 'px-2 py-1 border border-black rounded bg-green-500 delete'
     container.appendChild(card);
     card.appendChild(title);
     card.appendChild(img);
@@ -53,20 +61,29 @@ function display(library) {
     card.appendChild(pages);
     card.appendChild(read);
     card.appendChild(markAs);
+    card.appendChild(del);
+    del.addEventListener('click', remove);
+    function remove(){
+      removeFromLibrary(element.title);
+    }
+    markAs.addEventListener('click', change);
+    function change(){
+      if (element.read == true) {
+        element.read = false;
+      } else {
+        element.read = true;
+      }
+      saveLocal();
+      location.reload();
+    }
   })
 }
-
-display(myLibrary)
 
 // Modal functions.
 
 let modal = document.getElementById('modal');
 let btn = document.getElementById('btn');
 let span = document.getElementsByClassName("close")[0];
-
-const form = document.querySelector(".js-popup-form");
-form.addEventListener("submit", addBook);
-
 
 btn.onclick = function() {
   modal.style.display = "block";
@@ -82,19 +99,18 @@ window.onclick = function(event) {
   }
 }
 
-const addBtn = document.querySelector('#addB');
-
-btn.addEventListener('click', addNewBook());
-
 function addNewBook() {
-  let title = document.getElementById('title')
-  let author = document.getElementById('title')
-  let pages = document.getElementById('title')
-  let read = document.getElementById('title')
+  let inputs = document.getElementsByTagName('input');
+  let title = inputs[0].value
+  let author = inputs[1].value;
+  let pages = inputs[2].value;
+  let read = inputs[3].checked;
   let book = new Book(title, author, pages, read);
   addBookToLibrary(book);
-  localStorage.setItem(book);
 }
+
+const addBtn = document.querySelector('#addB');
+addBtn.addEventListener('click', addNewBook);
 
 // Local Storage
 
@@ -102,13 +118,4 @@ function saveLocal() {
   localStorage.setItem("myLibrary", JSON.stringify(myLibrary));
 }
 
-
-const csDestyled = new Book('Computer Science Destiled', 'Wladston Ferreira Filho', 165, true);
-const hpOne = new Book('Harry Potter and The Philosopher Stone', 'JK Rowlling', 150, true);
-const hpTwo = new Book('Harry Potter and The Chamber of Secrets', 'JK Rowlling', 250, true);
-const hpThree = new Book('Harry Potter and The Azkaban Prisioner', 'JK Rowlling', 350, true);
-
-addBookToLibrary(csDestyled);
-addBookToLibrary(hpOne);
-addBookToLibrary(hpTwo);
-addBookToLibrary(hpThree);
+display(myLibrary);
